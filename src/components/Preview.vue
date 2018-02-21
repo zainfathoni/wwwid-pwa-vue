6664b58d3d7f7e62d0b52967dc92d76a20ac9d15
Zain Fathoni <zain.fathoni@gmail.com>
2018-02-22 00:17:36 +0800

    Add Preview component

---
+++ src/components/Preview.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="preview">
+    <div class="img">
+      <img :src="article.thumbnail" :alt="article.slug">
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    article: {
+      required: true
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld v-if="loading" msg="Welcome to Your Vue.js App"/>
+    <ul v-else>
+      <li v-for="article in articles" :key="article.guid">
+        <preview :article="article" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-const API_URL = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fwwwid'
+const API_URL =
+  'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fwwwid'
 const PARAGRAPH = /<p>.*.<\/p>\n</g
 
 import HelloWorld from '@/components/HelloWorld.vue'
+import Preview from '@/components/Preview.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Preview
   },
   data() {
     return {
@@ -22,22 +30,25 @@ export default {
       articles: []
     }
   },
-  mounted () {
+  mounted() {
     this.fetchArticles()
   },
   methods: {
     fetchArticles() {
       this.loading = true
       fetch(API_URL)
-      .then(resp => resp.json())
-      .then(data => {
-        this.articles = data.items.map(item => {
-          item.contentView = item.content.match(PARAGRAPH).shift().slice(0, -1)
-          item.slug = item.link.split('/').pop()
-          return item
+        .then(resp => resp.json())
+        .then(data => {
+          this.articles = data.items.map(item => {
+            item.contentView = item.content
+              .match(PARAGRAPH)
+              .shift()
+              .slice(0, -1)
+            item.slug = item.link.split('/').pop()
+            return item
+          })
+          this.loading = false
         })
-        this.loading = false
-      })
     }
   }
 }
