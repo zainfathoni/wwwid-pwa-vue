a2eec0bb2a6212967e41f5910f7307bce51fc647
Zain Fathoni <zain.fathoni@gmail.com>
2018-02-21 23:47:17 +0800

    Add fetchArticles()

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,39 @@
 </template>
 
 <script>
-// @ is an alias to /src
+const API_URL = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fwwwid'
+const PARAGRAPH = /<p>.*.<\/p>\n</g
+
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      loading: true,
+      articles: []
+    }
+  },
+  mounted () {
+    this.fetchArticles()
+  },
+  methods: {
+    fetchArticles() {
+      this.loading = true
+      fetch(API_URL)
+      .then(resp => resp.json())
+      .then(data => {
+        this.articles = data.items.map(item => {
+          item.contentView = item.content.match(PARAGRAPH).shift().slice(0, -1)
+          item.slug = item.link.split('/').pop()
+          return item
+        })
+        this.loading = false
+      })
+    }
   }
 }
 </script>
