af04646cc837bf6ca97e6bc4166dbf99b42c99bb
Zain Fathoni <zain.fathoni@gmail.com>
2018-02-23 19:09:45 +0800

    Merge to gh-pages ==> 2018-2-23 19:09:35

---
+++ precache-manifest.2c21290ca936668124a0a9f1ab441976.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1885c6601912c23604ec",
+    "url": "/js/vendor.1885c660.js"
+  },
+  {
+    "revision": "ce28c628eea246b643c9",
+    "url": "/js/manifest.ce28c628.js"
+  },
+  {
+    "revision": "02ff30a02d9c91b6028f",
+    "url": "/js/app.02ff30a0.js"
+  },
+  {
+    "revision": "f0003f47d86273bca33b4b6e16bfbfae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02ff30a02d9c91b6028f",
+    "url": "/css/app.62d2f6a7.css"
+  }
+];
